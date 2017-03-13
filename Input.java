d4d7c8f743ab7d60a62961aec567ac667bc6fb89
Irena <iren.poskiene@gmail.com>
2017-03-13 18:09:24 +0200

    Naujas

---
+++ Input.java
@@ -0,0 +1,16 @@
+public static void main (String args []){
+
+
+Scanner sc = new Scanner (System.in);
+
+System.out.println ("enter something");
+
+String text = sc.next();
+
+
+System.out.println ("You enter+ "+ text); 
+
+
+
+}
+
