6404b96cf7c492d7adef02fbcaf066b85f70ea03
Irena <iren.poskiene@gmail.com>
2017-03-13 18:19:23 +0200

    naujas pakeitimas

--- HelloWorld.java
+++ HelloWorld.java
@@ -11,6 +11,17 @@ public class HelloWorld {
 		System.out.println("HELLO UGLY WORLD!");
 System.out.printLn("The World is not ungly");
 
+Scanner sc = new Scanner (System.in);
+
+System.out.println ("enter your name"); 
+
+String text = sc.next(); 
+
+System.out.println ("Hello+ "+ text+ "!"); 
+
+
+
+}
 
 	}
 }
